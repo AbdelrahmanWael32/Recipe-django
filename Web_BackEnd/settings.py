3d101cfded77108c2c6194b8c3d804d216d68945
aromatic32 <abdelrahman.wael1233@gmail.com>
2026-05-09 16:19:46 +0300

    moved all app static files to 1 static folder

--- Web_BackEnd/settings.py
+++ Web_BackEnd/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Accounts.apps.AuthConfig',
+    'Accounts',
     'recipes',
 ]
 
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # tells Django to look in your project-level /static folder
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic later
