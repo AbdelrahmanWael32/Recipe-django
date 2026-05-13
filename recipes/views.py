405a8c12dd43e9c71c9e341199ffe9a2170b60ec
morad <moradtarek67@gmail.com>
2026-05-13 14:11:11 +0300

    switched to django for the search bar

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,16 @@
+from django.db.models import Q
 from django.shortcuts import render
 from .models import Recipe
 
 def all_recipes_view(request):
+    query = request.GET.get('search', '')
     recipes = Recipe.objects.all()
-    return render(request, 'all_recipes.html', {'recipes': recipes})
+
+    if query:
+        recipes = recipes.filter(Q(name__icontains=query) |
+                                 Q(course_type__icontains=query) |
+                                 Q(difficulty__icontains=query))
+    return render(request, 'all_recipes.html', {'recipes': recipes, "query": query })
 
 
 
